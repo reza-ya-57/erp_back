6b635c3556843d77b0fa01a7dc70b4373c8f5923
reza-yari <rezayari.57@gmail.com>
2022-12-24 01:13:39 +0330

    add age to customuser

--- erp/users/admin.py
+++ erp/users/admin.py
@@ -9,6 +9,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["email", "username",]
+    list_display = ["email", "username", "age"]
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- erp/users/forms.py
+++ erp/users/forms.py
@@ -4,13 +4,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-
+    age = forms.IntegerField(label='age')
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ("username", "email" , "age")
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ("username", "email" , "age")
